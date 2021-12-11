7e0e1cae334e467ea762f813c1f22a1ab4cc3f8a
Justyna Pruś <justyna.p.prus@gmail.com>
2021-12-11 11:31:10 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const form = document.querySelector("form");
+const booksList = document.querySelector("ul");
+
+const storedBooksList = localStorage.getItem("booksList");
+let number = 0;
+
+console.log(booksList);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const read = [...document.getElementsByName("read")];
+  const category = [...document.getElementsByName("category")];
+
+  const titleValue = title.value;
+  const authorValue = author.value;
+
+  if (titleValue === "") {
+    alert("Tytuł nie może być pusty!");
+  }
+
+  if (authorValue.length < 3) {
+    alert("Autor musi zawierać min 3 znaki!");
+  }
+
+  let readValue = "";
+  let categoryValue = "";
+
+  for (i = 0; i < read.length; i++) {
+    if (read[i].checked) {
+      readValue = read[i].value;
+    }
+  }
+
+  for (i = 0; i < category.length; i++) {
+    if (category[i].checked) {
+      categoryValue = category[i].value;
+    }
+  }
+
+  number++;
+
+  const itemBook = document.createElement("li");
+  itemBook.innerHTML = `
+  <p class="number">${number}.</p>
+  <p class="cell">${titleValue}</p>
+  <p class="cell">${authorValue}</p>
+  <p class="smaller_cell">${readValue}</p>
+  <p class="cell">${categoryValue}</p>`;
+
+  booksList.appendChild(itemBook);
+  localStorage.setItem("booksList", JSON.stringify(booksList));
+  console.log(booksList);
+});
